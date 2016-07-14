8f8d35e8e0df83278b03c409d3be796151fe44cb
Joep Dumont <xorjoep@gmail.com>
2016-07-14 16:01:47 +0200

    added hashtag to RGB function #pls

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -6,7 +6,7 @@ var currentBlock;
 RGBToHex = function(r,g,b){
     var bin = r << 16 | g << 8 | b;
     return (function(h){
-        return new Array(7-h.length).join("0")+h
+        return "#" + new Array(7-h.length).join("0")+h
     })(bin.toString(16).toUpperCase())
 }
 
