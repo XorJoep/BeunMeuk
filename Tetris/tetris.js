c7b99ce4d8adb5f6085d45bcbec887c3ddb7a322
Joep Dumont <xorjoep@gmail.com>
2016-07-14 14:55:57 +0200

    hallo

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -1,8 +1,39 @@
-var getBlock = function(number) {
-	
-	block = {
-		sizeX : number
+var getRandomBlock = function() {
+	var 
+	block {
+		type : Math.floor(Math.random() * 7),
+		color : 
 	}
-
-	return block;
 }
+
+
+// var newBlock = function(blockType) {
+// 	this.x = 0;
+// 	this.y = 3;
+// 	this.rotation = 0;
+
+
+// 	return block;
+// }
+
+// var Blocks = {
+// 	O : 0,
+// 	I : 1,
+// 	J : 2,
+// 	L : 3,
+// 	S : 4,
+// 	T : 5,
+// 	Z : 6,
+// 	prop: {
+// 		0: {
+// 			code: "O",
+// 			rotatable: false,
+// 			form: [false, false, false, false,
+// 				   true, true, true, true]
+// 			},
+// 		1: {
+// 			code: "I",
+// 			rotatable: true
+// 		}
+// 	}
+// }
