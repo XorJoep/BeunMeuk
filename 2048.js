e88e79ea617b66e45ab926c68de8a2a7e51b24e1
wroel <WytzeR@topdesk.com>
2016-07-14 11:56:53 +0200

    check for legal moves

--- 2048.js
+++ 2048.js
@@ -36,20 +36,27 @@ document.onkeydown = checkKey;
 
 function checkKey(e) {
   e = e || window.event;
-  if(e.keyCode == '38'){
+  if(e.keyCode == '38' && checkIfLegalMove("up")){
     pressUp();
+    addNewTile();
+    showBoard();
   }
-  else if (e.keyCode == '40') {
+  else if (e.keyCode == '40' && checkIfLegalMove("down")) {
     pressDown();
+    addNewTile();
+    showBoard();
   }
-  else if (e.keyCode == '37') {
+  else if (e.keyCode == '37' && checkIfLegalMove("left")) {
     pressLeft();
+    addNewTile();
+    showBoard();
   }
-  else if (e.keyCode == '39') {
+  else if (e.keyCode == '39' && checkIfLegalMove("right")) {
     pressRight();
+    addNewTile();
+    showBoard();
   }
-  addNewTile();
-  showBoard();
+
 }
 
 var pressDown = function(){
@@ -155,4 +162,43 @@ var addNewTile = function(){
   }
   var randomTile = emptySpaces[Math.floor(Math.random() * emptySpaces.length)];
   grid[randomTile[0]][randomTile[1]] = 2;
+}
+var checkIfLegalMove = function(direction){
+  if(direction === "down"){
+    for(var i = 0; i < 3; i++){
+      for(var j = 0; j < 4; j++){
+        if((grid[i][j] !== 0 && grid[i + 1][j] === 0) || grid[i][j] === grid[i + 1][j]){
+          return true;
+        }
+      }
+    }
+  }
+  if(direction === "up"){
+    for(var i = 3; i > 0; i--){
+      for(var j = 0; j < 4; j++){
+        if((grid[i][j] !== 0 && grid[i - 1][j] === 0) || grid[i][j] === grid[i - 1][j]){
+          return true;
+        }
+      }
+    }
+  }
+  if(direction === "left"){
+    for(var i = 0; i < 4; i++){
+      for(var j = 3; j > 0; j--){
+        if((grid[i][j] !== 0 && grid[i][j - 1] === 0) || grid[i][j] === grid[i][j - 1]){
+          return true;
+        }
+      }
+    }
+  }
+  if(direction === "right"){
+    for(var i = 0; i < 4; i++){
+      for(var j = 0; j < 3; j++){
+        if((grid[i][j] !== 0 && grid[i][j + 1] === 0) ||  grid[i][j] === grid[i][j + 1]){
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
