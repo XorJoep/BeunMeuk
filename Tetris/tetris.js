91f876865c505d8ce006f1c60e1fdfea10f7863b
wroel <WytzeR@topdesk.com>
2016-07-14 14:55:07 +0200

    add some first functions

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -1,3 +1,24 @@
+var grid = [];
+var startNewGame = function(){
+	for(var i = 0; i < 8; i++){
+		var row = [];
+		for(var j = 0; j < 16; j++){
+			row.push(0);
+		}
+		grid.push(row);
+	}
+}
+var showBoard = function(){
+	var board = document.getElementById("board");
+  var rows = board.getElementsByClassName("row");
+  var tiles;
+  for (var i = 0; i < rows.length; i++) {
+    tiles = rows[i].getElementsByClassName("tile");
+    for (var j = 0; j < tiles.length; j++) {
+      tiles[j].style.background = grid[i][j] ?  currentBlock.color : "lightgrey";
+    }
+	}
+}
 var getBlock = function(number) {
 
 	block = {
@@ -6,18 +27,3 @@ var getBlock = function(number) {
 
 	return block;
 }
-
-var makeBoard = function(){
-	var board = document.getElementById("board");
-  var rows = board.getElementsByClassName("row");
-  var tiles;
-  for (var i = 0; i < rows.length; i++) {
-    tiles = rows[i].getElementsByClassName("tile");
-    for (var j = 0; j < tiles.length; j++) {
-      tiles[j].style.background = grid[i][j] ? "rgb(255," + currentBlock.color : "lightgrey";
-    }
-}
-
-var interval = setInterval(function() {
-    moveCurrentBlock();
-}, 1000);
