1ce8a65990d188ce13e957c7d4adda356472115c
Joep Dumont <xorjoep@gmail.com>
2016-07-15 10:58:28 +0200

    meuk added

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -22,6 +22,7 @@ var startNewGame = function(){
 	gameIsStarted = true;
 	showBoard();
 }
+
 function checkKey(e) {
   e = e || window.event;
   if(e.keyCode == '38' ) {//&& checkIfLegalMove("up")){
@@ -45,6 +46,7 @@ function checkKey(e) {
     showBoard();
   }
 }
+
 var showBoard = function(){
   var board = document.getElementById("board");
   var rows = board.getElementsByClassName("row");
@@ -79,7 +81,8 @@ var pressRight = function() {
 	grid[currentBlock.form[2][0] + currentBlock.y][currentBlock.form[2][1] + currentBlock.x] = currentBlock.color;
 	grid[currentBlock.form[3][0] + currentBlock.y][currentBlock.form[3][1] + currentBlock.x] = currentBlock.color;
 }
-var checkIfLegalMove = function(dir){
+
+var checkIfLegalMove = function(dir) {
 	if(dir === "left"){
 		for(var i = 0; i < 4; i++){
 			if(currentBlock.form[i][1] + currentBlock.x === 0){
@@ -96,6 +99,7 @@ var checkIfLegalMove = function(dir){
 	}
 	return true;
 }
+
 var getRandomBlock = function() {
 	var type = Math.floor(Math.random() * 8);
 	block = new Block(Blocks[Blocks[type]]);
@@ -110,15 +114,17 @@ var rotateBlock = function(block, orientation) {
      return block;
    }
 	for (var i = 0; i < block.form.length; i++) {
-    grid[currentBlock.y + currentBlock.form[i][0]][currentBlock.x + currentBlock.form[i][1]] = 0;
-		block.form[i] = multiply([block.form[i]], orientation ? [[0, 1], [-1, 0]] : [[0, -1], [1, 0]])[0];
+    	grid[block.y + block.form[i][0]][block.x + block.form[i][1]] = 0;
+    	rotatedForm = multiply([block.form[i]], orientation ? [[0, 1], [-1, 0]] : [[0, -1], [1, 0]])[0];
+		if (rotatedForm[0]+block.x)
+		block.form[i] = 
+		// debugger;
 	}
-  grid[currentBlock.form[0][0] + currentBlock.y][currentBlock.x + currentBlock.form[0][1]] = currentBlock.color;
-  grid[currentBlock.y + currentBlock.form[1][0]][currentBlock.x + currentBlock.form[1][1]] = currentBlock.color;
-  grid[currentBlock.y + currentBlock.form[2][0]][currentBlock.x + currentBlock.form[2][1]] = currentBlock.color;
-  grid[currentBlock.y + currentBlock.form[3][0]][currentBlock.x + currentBlock.form[3][1]] = currentBlock.color;
+	grid[block.form[0][0] + block.y][block.x + block.form[0][1]] = block.color;
+	grid[block.y + block.form[1][0]][block.x + block.form[1][1]] = block.color;
+	grid[block.y + block.form[2][0]][block.x + block.form[2][1]] = block.color;
+	grid[block.y + block.form[3][0]][block.x + block.form[3][1]] = block.color;
 	return block;
-
 }
 
 var checkIfMoveable = function(){
@@ -139,8 +145,10 @@ var getBlocksThatCanCollide = function(){
   var fourthBlock = currentBlock.form[3];
   for(var i = i; i < 4; i++){
     if(currentBlock.form[0][0] === currentBlock.form[i][0])
+    	a = 1;
   }
 }
+
 var interval = setInterval(function() {
 	if(gameIsStarted && normalSpeed && checkIfMoveable()){
 		grid[currentBlock.y + currentBlock.form[0][0]][currentBlock.x + currentBlock.form[0][1]] = 0;
@@ -153,9 +161,9 @@ var interval = setInterval(function() {
 		grid[currentBlock.y + currentBlock.form[2][0]][currentBlock.x + currentBlock.form[2][1]] = currentBlock.color;
 		grid[currentBlock.y + currentBlock.form[3][0]][currentBlock.x + currentBlock.form[3][1]] = currentBlock.color;
 		showBoard();
-		// debugger;
 	}
 }, 1000);
+
 var interval = setInterval(function() {
 	if(gameIsStarted && !normalSpeed && checkIfMoveable()){
 		grid[currentBlock.y + currentBlock.form[0][0]][currentBlock.x + currentBlock.form[0][1]] = 0;
