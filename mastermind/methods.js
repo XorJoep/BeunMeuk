896f7a7f2cbfadcf3ba17575cc9562acf5857896
HSIHF <tom.postema.463@student.lu.se>
2016-08-06 12:57:25 +0200

    Made some changes in order to adhere to the coding conventions...

--- mastermind/methods.js
+++ mastermind/methods.js
@@ -3,28 +3,28 @@ var solution = [0, 0, 0, 0];
 var startOfRow = 0;
 var row = 1;
 
-function startMasterGame(){
+function startMasterGame() {
   initializeArray();
   randomizeSolution();
   console.log(solution);
-
   var s = "";
-  for(var i = 0; i < 10; i++){
+
+  for(var i = 0; i < 10; i++) {
     s += '<div class="MasterRow">';
-    for(var j = 0; j < 4; j++){
+    for(var j = 0; j < 4; j++) {
       s += '<div id="' + i + j + '" class="MasterTiles" onclick="changeColor(this.id)"></div>';
     }
     s += '</div>';
-
   }
-  document.getElementById("MasterBoard").innerHTML = s;
 
+  document.getElementById("MasterBoard").innerHTML = s;
   var t = "";
-  for(var r = 1; r < 11; r++){
+
+  for(var r = 1; r < 11; r++) {
     t += '<div class="MasterRow">';
-    for(var i = 0; i < 2; i++){
+    for(var i = 0; i < 2; i++) {
       t += '<div class="MasterCheckTiles">';
-      for(var j = 0; j < 2; j++){
+      for(var j = 0; j < 2; j++) {
         t += '<div id ="' + r + i + j + '" class="CheckTiles"></div>';
       }
       t += '</div>';
@@ -34,14 +34,14 @@ function startMasterGame(){
   document.getElementById("MasterCheck").innerHTML = t;
 }
 
-function initializeArray(){
-  for(var i = 0; i < 100; i++){
+function initializeArray() {
+  for(var i = 0; i < 100; i++) {
     counter[i] = -1;
   }
 }
 
 
-function changeColor(elem){
+function changeColor(elem) {
   counter[parseInt(elem)]++;
   if(counter[parseInt(elem)] === 5) {
   	counter[parseInt(elem)] = 0;
@@ -67,21 +67,21 @@ function changeColor(elem){
 
 }
 
-function checkMaster(){
+function checkMaster() {
   var correct = 0;
   var whites = 0;
   var currentRow = [counter[startOfRow], counter[startOfRow+1],counter[startOfRow+2],counter[startOfRow+3]];
   var done = [false, false, false, false];
   var check = [true, true, true, true];
 
-  for(var i = 0; i < solution.length; i++){
+  for(var i = 0; i < solution.length; i++) {
     if(currentRow[i] === -1) { //invalid input
       return; 
     }
   }
 
-  for(var i = 0; i < solution.length; i++){
-    if(solution[i] === currentRow[i]){
+  for(var i = 0; i < solution.length; i++) {
+    if(solution[i] === currentRow[i]) {
       correct++;
       done[i] = true;
       check[i] = false;
@@ -111,13 +111,13 @@ function checkMaster(){
 
 }
 
-function randomizeSolution(){
-  for(var i = 0; i < solution.length; i++){
+function randomizeSolution() {
+  for(var i = 0; i < solution.length; i++) {
     solution[i] = Math.floor(Math.random()*5);
   }
 }
 
-function colorCheckTiles(correct, whites){
+function colorCheckTiles(correct, whites) {
   var id = row * 100;
   row++;
   var idArray = [id, id + 1, id + 10, id + 11];
